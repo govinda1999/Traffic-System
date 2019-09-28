54c421f591652492ccc7393449c0237d777ea4ba
govinda1999 <gspatel61199@gmail.com>
2019-09-29 01:57:00 +0530

    Working onauth routes

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost/trafficsystem';
+const auth = require('./routes/auth');
+const data = require('./routes/data');
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
+mongoose.connect(
+  url,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, result) => {
+    if (err) {
+      console.log('Error at connecting Database');
+    } else {
+      console.log('Database Connected');
+    }
+  }
+);
 
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
+app.use(cors);
+
+app.use('/api/auth', auth);
+app.use('/api/data', data);
+
+app.use((req, res, next) => {
+  const error = new Error('Page Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    Error_status: error.status || 500,
+    message: error.message
+  });
+});
 
 app.listen(port, () => console.log(`Hello Listening at ${port}`));
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,38 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-router.get('');
-
+const Traffic = require('../models/Traffic');
+router.post('/register', (req, res) => {
+  const TrafficData = new Traffic({
+    _id: mongoose.Types.ObjectId(),
+    location: req.body.location
+  });
+  TrafficData.save()
+    .exec()
+    .then(docs => {
+      res.status(200).json({
+        message: 'Traffic Signal Registered Succesfully'
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Traffic Signal Not Registered Succesfully'
+      });
+    });
+});
+router.get('/', (req, res) => {
+  Traffic.find()
+    .exec()
+    .then(docs => {
+      res.status(200).json({
+        count: docs.length,
+        data: docs
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Error'
+      });
+    });
+});
 module.exports = router;
